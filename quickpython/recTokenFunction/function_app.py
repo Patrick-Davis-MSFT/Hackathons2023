c7ef5a3a4de8c4903e963cd14089b2b638e17d72
Patrick C Davis <patrickdavis@microsoft.com>
2024-08-30 13:07:13 -0400

    updated logging

--- quickpython/recTokenFunction/function_app.py
+++ quickpython/recTokenFunction/function_app.py
@@ -4,19 +4,61 @@ import json
 import logging
 import tiktoken
 import io
+import requests
+import hashlib
+import hmac
+import base64
+import os
 
 app = func.FunctionApp()
 model = ""
+workspace_id = os.getenv('LOG_ANALYTICS_WORKSPACE_ID','NEED LOG_ANALYTICS_WORKSPACE_ID')
+primary_key = os.getenv('LOG_ANALYTICS_PRIMARY_KEY', 'NEED LOG_ANALYTICS_PRIMARY_KEY')
+log_type = 'CustomUsageLog'
+
+def send_to_log_analytics(workspace_id, primary_key, log_type, json_data):
+    customer_id = workspace_id
+    shared_key = primary_key
+
+    method = 'POST'
+    content_type = 'application/json'
+    resource = '/api/logs'
+    rfc1123date = datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
+    content_length = len(json_data)
+
+    signature = build_signature(customer_id, shared_key, rfc1123date, content_length, method, content_type, resource)
+    uri = f'https://{customer_id}.ods.opinsights.azure.com{resource}?api-version=2016-04-01'
+
+    headers = {
+        'Content-Type': content_type,
+        'Authorization': signature,
+        'Log-Type': log_type,
+        'x-ms-date': rfc1123date
+    }
+
+    response = requests.post(uri, headers=headers, data=json_data)
+    if response.status_code >= 200 and response.status_code <= 299:
+        print('Accepted')
+    else:
+        print(f"Response code: {response.status_code}")
+
+def build_signature(customer_id, shared_key, date, content_length, method, content_type, resource):
+    x_headers = f'x-ms-date:{date}'
+    string_to_hash = f'{method}\n{content_length}\n{content_type}\n{x_headers}\n{resource}'
+    bytes_to_hash = bytes(string_to_hash, encoding='utf-8')
+    decoded_key = base64.b64decode(shared_key)
+    encoded_hash = base64.b64encode(hmac.new(decoded_key, bytes_to_hash, digestmod=hashlib.sha256).digest()).decode('utf-8')
+    authorization = f'SharedKey {customer_id}:{encoded_hash}'
+    return authorization
+
 @app.route(route="promptCounter/{subscription}", auth_level=func.AuthLevel.FUNCTION)
 def promptCounter(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('promptCounter HTTP trigger function processed a request.')
-
+    logging.info('promptCounter HTTP process incoming request.')
+    logObj = {}
     # Get the subscription parameter from the route
     subscription = req.route_params.get('subscription')
-    logging.info(f"Subscription: {subscription}")
     try:
         req_body = req.get_json()
-        logging.info(f"Request body: {req_body}")
     except ValueError:
         return func.HttpResponse(
             "Invalid JSON in request body.",
@@ -26,12 +68,16 @@ def promptCounter(req: func.HttpRequest) -> func.HttpResponse:
     messages = req_body.get('messages') if req_body else None
     model = req_body.get('model') if req_body else None
     
-    logging.info(f"messages: {messages}")
-    logging.info(f"model: {model}")
-
     if messages:
         num_tokens = num_tokens_from_messages(messages=messages, model=model)
         logging.info(f"reported tokens from user prompt: {num_tokens}")
+        logObj["estimated_token_count"] = num_tokens
+        logObj["subscription"] = subscription
+        logObj["model"] = model
+        logObj["direction"] = 'toAOAI'
+        logObj["stream"] = req_body.get('stream')
+        # Send usage data to Log Analytics
+        send_to_log_analytics(workspace_id, primary_key, log_type, json.dumps(logObj))
         return func.HttpResponse(
             json.dumps({"token_count": num_tokens}),
             status_code=200,
@@ -45,6 +91,8 @@ def promptCounter(req: func.HttpRequest) -> func.HttpResponse:
 
 def getOutputStream(req: func.HttpRequest):
     assistant_message = ""
+    model = ""
+    retValue = {}
     try:
         logging.info("Try to get Stream message")
         req_body_stm = req.get_body().decode('utf-8') 
@@ -64,10 +112,12 @@ def getOutputStream(req: func.HttpRequest):
                 
                 except json.JSONDecodeError as e:
                     logging.error(f"Failed to parse JSON: {e}")
-        logging.info(f"assistant_message: {assistant_message}")
     except:
         logging.info("Failed to get Stream message")
-    return assistant_message
+
+    retValue["assistant_message"] = assistant_message
+    retValue["model"] = model
+    return retValue
 
 def getOutputBulk(req: func.HttpRequest):
     assistant_message = ""
@@ -83,28 +133,38 @@ def getOutputBulk(req: func.HttpRequest):
     choices = req_body.get('choices') if req_body else None
     model = req_body.get('model') if req_body else None
     
-    logging.info(f"choices: {choices}")
-    logging.info(f"model: {model}")
 
     if choices and isinstance(choices, list) and len(choices) > 0:
         assistant_message = choices[0].get('message', {}).get('content', "")
-        logging.info(f"bulk messages: {assistant_message}")
     return assistant_message
 
-@app.route(route="pmtCntOutBulk/{subscription}", auth_level=func.AuthLevel.FUNCTION)
-def pmtCntOutBulk(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('promptCounterOutBulk HTTP trigger function processed a request.')
-    
+@app.route(route="pmtCntOut/{subscription}", auth_level=func.AuthLevel.FUNCTION)
+def pmtCntOut(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('promptCounterOutBulk HTTP process outbound request.')
+    logObj = {}
+    stream = True
     # Get the subscription parameter from the route
     subscription = req.route_params.get('subscription')
-    logging.info(f"Subscription: {subscription}")
     assistant_message = ""
     # Read the MIME type of the request
-    assistant_message = getOutputStream(req)
+    outStreamRes = getOutputStream(req)
+    if outStreamRes:
+        assistant_message = outStreamRes.get("assistant_message")
+        model = outStreamRes.get("model")
+
     if assistant_message =="":
         assistant_message = getOutputBulk(req)
+        stream = False
+        try: 
+            req_body = req.get_json()
+            if req_body.get('usage'):
+                usage = req_body.get('usage')
+                model = req_body.get('model')
+                if usage:
+                    logObj=usage
+        except:
+            logging.info("Failed to get usage")
         
-    logging.info(f"assistant_message main function: {assistant_message}")
     if assistant_message:
         try:
             encoding = tiktoken.encoding_for_model(model)
@@ -113,6 +173,14 @@ def pmtCntOutBulk(req: func.HttpRequest) -> func.HttpResponse:
             encoding = tiktoken.get_encoding("cl100k_base")
         num_tokens = len(encoding.encode(assistant_message))
         logging.info(f"reported tokens from response: {num_tokens}")
+
+        logObj["estimated_token_count"] = num_tokens
+        logObj["subscription"] = subscription
+        logObj["model"] = model
+        logObj["direction"] = 'fromAOAI'
+        logObj["stream"] = stream
+        # Send usage data to Log Analytics
+        send_to_log_analytics(workspace_id, primary_key, log_type, json.dumps(logObj))
         return func.HttpResponse(
             json.dumps({"token_count": num_tokens}),
             status_code=200,
